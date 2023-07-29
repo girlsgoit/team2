dc8c7417357ca2a1d4ac11554861f5913c171874
delia013 <sidletchidelia2009@gmail.com>
2023-07-29 13:35:23 +0300

    reviews

--- frontDinamic/grupul2/src/components/Reviews.vue
+++ frontDinamic/grupul2/src/components/Reviews.vue
@@ -1,4 +1,50 @@
-<script >
+<script setup>
+
+import { ref } from 'vue';
+import { collection, addDoc, getDocs} from "firebase/firestore";
+import { db } from '../firebase';
+
+const name = ref('');
+const content = ref('');
+
+const SubReview = async () => {
+  try {
+    const docRef = await addDoc(collection(db, "goji-reviews"), {
+      name: name.value,
+      content: content.value,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+};
+
+let review = [];
+
+// Retrieve data from a Firestore subcollection called "goji-reviews"
+async function fetchReviews() {
+  try {
+    // Query a reference to the subcollection
+    const querySnapshot = await getDocs(collection(db, "goji-reviews"));
+
+    // Loop through the query snapshot to get each document
+    querySnapshot.forEach((doc) => {
+
+      // Push name and content to the review array
+      review.push({
+        name: doc.data().name,
+        content: doc.data().content
+      });
+    });
+
+    console.log("Review array:", review); // Log the populated review array
+  } catch (error) {
+    console.error("Error fetching reviews: ", error);
+  }
+}
+
+// Call the function to fetch and log the reviews
+fetchReviews();
 
 </script>
 
@@ -8,12 +54,19 @@
 <div class="container">
   <h1>Restaurant Reviews</h1>
   <div id="reviews-container"></div>
-  <form id="review-form">
-    <input type="text" id="name" placeholder="Your Name" required/>
-    <textarea id="review" placeholder="Write your review here" required></textarea>
+  <form id="review-form" @submit.prevent="SubReview">
+    <input v-model="name" placeholder="Your name" />
+    <input v-model="content" placeholder="Your review" />
     <button type="submit">Submit Review</button>
   </form>
 </div>
+<section>
+  <div v-for="rev in review">
+    <h2>{{ rev.name }}</h2>
+    <p>{{ rev.content }}</p>
+  </div>
+</section>
+<br/>
 
 </body>
 </template>
--- frontDinamic/grupul2/src/firebase/index.js
+++ frontDinamic/grupul2/src/firebase/index.js
@@ -1,8 +1,8 @@
 
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,3 +27,33 @@ export {
     db
 }
 
+// const reviews = ref([]);
+
+// export async function getReviews() {
+//   const querySnapshot = await getDocs(collection(db, "goji-reviews"));
+// querySnapshot.forEach((doc) => {
+//   // doc.data() is never undefined for query doc snapshots
+//   console.log(doc.id, " => ", doc.data());
+// });
+// }
+
+// export async function addReviews(name, content){
+// // Add a new document with a generated id.
+// const docRef = await addDoc(collection(db, "goji-reviews"), {
+//   name: name,
+//   content: content
+// });
+// }
+ 
+
+// export async function deleteReviews(){ 
+//    const querySnapshot = await deleteDoc(doc(db, "goji-reviews", "DC"));
+
+// }
+
+// export const washingtonRef = doc(db, "goji-reviews", "DC");
+
+// // Set the "capital" field of the city 'DC'
+// await updateDoc(washingtonRef, {
+//   capital: true
+// });
