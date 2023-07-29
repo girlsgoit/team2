2f76187e8b1baa17c3fc59a923aa0911f4fd711a
delia013 <sidletchidelia2009@gmail.com>
2023-07-29 15:32:53 +0300

    commit

--- frontDinamic/grupul2/src/components/Bookings.vue
+++ frontDinamic/grupul2/src/components/Bookings.vue
@@ -1,4 +1,12 @@
 <template>
+     <header id="container1" >
+      
+      <RouterLink :to = "{name:'home'}"><button class="button1" style="margin-left:0%;margin-top: 150%;"><u><h1>Home</h1></u></button></RouterLink>
+      <RouterLink :to = "{name:'menu'}"><button class="button1" style="opacity:50%; margin-left: 90%;margin-top: 160%;"><h1>Menu</h1></button></RouterLink>
+      <RouterLink :to = "{name:'aboutUs'}"><button class="button1" style="opacity:50%; margin-left: 180%;margin-top: 150%;"><h1>About</h1></button></RouterLink>
+      <RouterLink :to = "{name:'reviews'}"><button class="button1"  style="opacity:50%; margin-left: 210%; margin-top: 112%;" ><h1>Reviews</h1></button></RouterLink>
+      <RouterLink :to = "{name:'bookings'}"><button id="button2"  style="opacity:50%;margin-left:180%; margin-top: 62%;"><h1>Book a table</h1></button></RouterLink>
+  </header>
   <form id="bookingForm">
     <label for="name">Name:</label>
     <input type="text" id="name" required>
@@ -17,6 +25,15 @@
 
     <button type="submit">Book Table</button>
   </form>
+  
+<footer>
+<div align="center"> 
+ <a href="https://www.facebook.com/gojistudioshk/"> <img class="sponsor" src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/1200px-Facebook_f_logo_%282021%29.svg.png" ></a>
+ <a href="https://www.instagram.com/lifesgoji/"> <img class="sponsor" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"></a>
+  <a href="https://twitter.com/Goji_investor"><img class="sponsor" src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png" ></a>
+</div>
+<div id="div4" align="center">&copy; All rights reserved - Goji Restobar 2023</div>
+</footer>
 </template>
 
 <script>
@@ -38,5 +55,12 @@
 </script>
 
 <style>
+#container1 {
 
+display: flex;
+justify-content: center;
+margin: 1%;
+margin-top:-5%;
+margin-left:-20%;
+}
 </style>
--- frontDinamic/grupul2/src/components/Menu1.vue
+++ frontDinamic/grupul2/src/components/Menu1.vue
@@ -5,6 +5,14 @@
 <template>
     
     <body>   
+        <header id="container1" >
+      
+      <RouterLink :to = "{name:'home'}"><button class="button1" style="margin-left:0%;margin-top: 150%;"><u><h1>Home</h1></u></button></RouterLink>
+      <RouterLink :to = "{name:'menu'}"><button class="button1" style="opacity:50%; margin-left: 90%;margin-top: 160%;"><h1>Menu</h1></button></RouterLink>
+      <RouterLink :to = "{name:'aboutUs'}"><button class="button1" style="opacity:50%; margin-left: 180%;margin-top: 150%;"><h1>About</h1></button></RouterLink>
+      <RouterLink :to = "{name:'reviews'}"><button class="button1"  style="opacity:50%; margin-left: 210%; margin-top: 112%;" ><h1>Reviews</h1></button></RouterLink>
+      <RouterLink :to = "{name:'bookings'}"><button id="button2"  style="opacity:50%;margin-left:180%; margin-top: 62%;"><h1>Book a table</h1></button></RouterLink>
+  </header>
         <section class="breakfast-section">
             <h1>                
                 BREAKFAST
@@ -307,11 +315,16 @@
 </div>
 
     </div>
+    <footer>
+<div align="center"> 
+ <a href="https://www.facebook.com/gojistudioshk/"> <img class="sponsor" src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/1200px-Facebook_f_logo_%282021%29.svg.png" ></a>
+ <a href="https://www.instagram.com/lifesgoji/"> <img class="sponsor" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"></a>
+  <a href="https://twitter.com/Goji_investor"><img class="sponsor" src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png" ></a>
+</div>
+<div id="div4" align="center">&copy; All rights reserved - Goji Restobar 2023</div>
+</footer>
     </body>
   
-=======
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
 
 
 </template>
@@ -336,6 +349,7 @@
   font-size:1.5rem;
   font-style:italic;
 }
+body{color:white}
 .container{
   width:100vw;
   display:flex;
@@ -368,7 +382,7 @@
   margin-bottom:-50px;
   height:150px;
   width:300px;
-  background-color:#1D1C20;
+  background-color:#1c201c;
   color:#fff;
   padding:20px;
 }
@@ -400,5 +414,13 @@
     font-size: 85px;
     padding: 70px;
     flex: auto;
+}
+#container1 {
+
+  display: flex;
+  justify-content: center;
+  margin: 1%;
+  margin-top:5%;
+  margin-left:-20%;
 }
 </style>
